37c5c2d181cee0b207be49f8303a67ea113b784c
harshrao26 <harshurao058@gmail.com>
2024-10-04 17:35:22 +0530

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,47 @@
 import React from "react";
-import Button from './Button'
+import Button from "./Button";
+import overlays from "../../public/overlays.png";
+import overlays2 from "../../public/overlays2.png";
 const Hero = () => {
   return (
     <div
-      className="w-full relative  py-20 md:py-40 flex flex-col items-center justify-center px-4 md:px-0 bg-cover overflow-hidden bg-center"
+      className="w-full relative bg-[#052449]  py-20 md:py-40 flex flex-col items-center justify-center px-4 md:px-0 bg-cover overflow-hidden bg-center"
       style={{
         backgroundImage:
           "url('htt://images.unsplash.com/photo-1520032525096-7bd04a94b5a4?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       }}
     >
       <div className="backdrop-blur- py-8 px-4 rounded-xl z-20 flex flex-col items-center justify-center ">
-        <h1 className="text-3xl md:text-5xl lg:text-6xl text-center max-w-4xl leading-tight">
-          <span className="text-blue-500">Your Premier</span> Partner for
+        <h1 className="text-3xl md:text-5xl lg:text-6xl text-white text-center max-w-4xl leading-tight">
+          <span className="text-blue-400">Your Premier</span> Partner for
           Monetizing Commercial Assets with{" "}
-          <span className="text-blue-500">Coworking Spaces</span>
+          <span className="text-blue-400">Coworking Spaces</span>
         </h1>
-        <p className="text-sm md:text-md lg:text-lg max-w-3xl text-center  py-4 capitalize text-gray-600">
+        <p className="text-sm md:text-md lg:text-lg max-w-3xl text-center  py-4 capitalize text-gray-100">
           Helping property owners, entrepreneurs, and real estate professionals
           transform and matchmake spaces into{" "}
-          <span className="text-blue-500">profitable coworking businesses</span>
+          <span className="text-blue-400">profitable coworking businesses</span>
         </p>
         <div className="w-full flex items-center justify-center">
           <Button name="Let's Talk" />
         </div>
       </div>
       {/* <img src="https://img.freepik.com/premium-vector/dashed-line-paper-airplane-route_536326-465.jpg?w=900" alt="" className="absolute z-10 ml-80 mt-80 mix-blend-darken p-4 "/> */}
-      <img src="https://www.freepnglogos.com/uploads/paper-plane-png/paper-plane-making-hammer-paper-airplane-that-flies-far-emerging-11.png" alt="" className="absolute z-10 md:w-full md:p-96 w-60 md:ml-[45vw] ml-[0vw] "/>
+      <img
+        src="https://www.freepnglogos.com/uploads/paper-plane-png/paper-plane-making-hammer-paper-airplane-that-flies-far-emerging-11.png"
+        alt=""
+        className="absolute z-10 md:w-full md:p-96 w-60 md:ml-[45vw] ml-[0vw] z-20 "
+      />
+      <img
+        src={overlays}
+        alt=""
+        className="absolute z-10 h-screen mr-[52vw] -mt-20   "
+      />
+      <img
+        src={overlays2}
+        alt=""
+        className="absolute z-10 h-screen ml-[52vw] -mt-20   "
+      />
     </div>
   );
 };
--- src/components/Profit.jsx
+++ src/components/Profit.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import Button from "./Button";
+import bg from '../../public/bg.webp'
 const Profit = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center  px-8 md:px-16 ">
+    <div
+      className="w-full flex flex-col items-center justify-center  px-8 md:px-16  "
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       {/* Text Section */}
-      <h1 className="text-3xl md:text-4xl text-center font-semibold leading-snug py-8 text-gray-900">
+      <h1 className="text-3xl md:text-4xl text-center font-semibold leading-snug pt-8 pb-4 text-gray-900">
         Maximize Your Asset’s Profit Potential in Just{" "}
         <span className="inline-block text-semibold text-white px-4 py-1 bg-blue-500">
           6 Months
         </span>
       </h1>
       <div className="flex justify-center w-full flex-col items-cener space-y-4 pb-8">
-        <h1 className="text-lg md:text-lg md:px-64 text-center leading-relaxed text-gray-700">
-          <span className="text-5xl  font-semibold font text-blue-600">
-            65%{" "} <br />
+        <h1 className="text-lg md:text-lg md:px-64 text-center leading-relaxed text-gray-100">
+          <span className="text-5xl  font-semibold font text-white">
+            65% <br />
           </span>
           of our clients have tripled their profits in the first 6 months by
           monetizing their assets with our proven strategies.
         </h1>
-        <h1 className="text-lg md:text-lg md:px-64 text-center leading-relaxed text-gray-700">
-          <span className="text-5xl  font-semibold font text-blue-600">
-            80%{" "} <br />
+        <h1 className="text-lg md:text-lg md:px-64 text-center leading-relaxed text-gray-100">
+          <span className="text-5xl  font-semibold font text-white">
+            80% <br />
           </span>
           Leveraging data from multiple brands, we’ve consistently turned
           underutilized spaces into high-performing coworking hubs. Achieve 80%
           Occupancy & Beyond.
         </h1>
-        
       </div>
-    
     </div>
   );
 };
